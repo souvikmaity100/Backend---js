8ed97bbe950c3754851d9d72165efe2f24d7c874
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-02-19 21:56:52 +0530

    Basic Data Modelling for Todos with Mongoose

---
+++ 03_dataModlingWithMongoose/models/todos/sub_todo.models.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const subTodoSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        complete: {
+            type: Boolean,
+            default: false
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,  // reference to the user who owns this todo
+            ref: 'User'                           
+        }
+    }, {timestamps: true}
+)
+
+export const SubTodo = mongoose.model( 'SubTodo', subTodoSchema ); 
---
+++ 03_dataModlingWithMongoose/models/todos/todo.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        complete: {
+            type: Boolean,
+            default: false
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,  // Reference to the User model
+            ref: "User"
+        },
+        subTodos: [
+            {
+                type: mongoose.Schema.Types.ObjectId,   // References the SubTodo model
+                ref: "SubTodo"
+            }
+        ] // Array of Sub-Todos
+    }, { timestamps: true }
+)
+
+export const Todo = mongoose.model("Todo", todoSchema)
