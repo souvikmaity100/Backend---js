ca993b1388296edac39ce59adbddfbadcee89ad4
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-02-20 22:51:36 +0530

    Connect to MongoDB Atlas

---
+++ 04_YoutubeClone/src/index.js
@@ -0,0 +1,56 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+ 
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// --------Connect to MongoDB----------
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+const app = express();
+
+( async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB Connected || DB Host: ${connectionInstance.connection.host}`);
+        app.on("error", (error)=>{
+            console.log("Express ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}.`);
+        });
+    } catch (error) {
+        console.error("MongoDB ERROR: ", error);
+        throw error
+    }
+})()
+
+*/
