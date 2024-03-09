60a981af7afa82149b3e6e7244961b39b673b72c
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-03-09 22:21:09 +0530

    add getWatchHistory endpoint and write nested Aggregation Pipeline

--- 04_YoutubeClone/src/controllers/user.controller.js
+++ 04_YoutubeClone/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uplodeOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -386,6 +387,52 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, channel[0], "Successfully fetched channel data"))
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return  res.status(200).json(new ApiResponse(200, user[0].watchHistory, "Successfully get the watch history"))
+})
+
 export { 
   registerUser,
   loginUser, 
@@ -396,4 +443,6 @@ export {
   updateAccountDetails, 
   updateUserAvatar, 
   updateUserCoverImage, 
-  getUserChannelProfile };
+  getUserChannelProfile,
+  getWatchHistory
+ };
