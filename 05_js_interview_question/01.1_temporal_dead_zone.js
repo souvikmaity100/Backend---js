6db44daa82632bf718b83a5b0e0fdcdf5208b213
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-04-15 22:38:23 +0530

    Temporal Dead zone

---
+++ 05_js_interview_question/01.1_temporal_dead_zone.js
@@ -0,0 +1,9 @@
+age = 50;
+
+console.log("Age is: ", age);
+
+let age = 25;
+
+console.log("Age is: ", age);
+
+/* A temporal dead zone (TDZ) is the area of a block where a variable is inaccessible until the moment the computer completely initializes it with a value. */
