70a050cf28c60d886dc3b70f73c78a6ba9819f3c
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-04-16 22:56:52 +0530

    modules in node js

---
+++ 06_node_js/01_hello_world/fun1.js
@@ -0,0 +1,9 @@
+
+function avg(a, b, c) {
+    return ((a+b+c)/3)
+}
+
+function add(a, b, c, d) {
+    return a+b+c+d
+}
+module.exports = {avg, add}
--- 06_node_js/01_hello_world/hello.js
+++ 06_node_js/01_hello_world/hello.js
@@ -1,2 +1,7 @@
-console.log("hello World from node js");
+const {avg, add} = require('./fun1.js')
+
+// console.log("hello World from node js");
+
+console.log(avg(12, 14, 16));
+console.log(add(12, 14, 16, 15));
 
