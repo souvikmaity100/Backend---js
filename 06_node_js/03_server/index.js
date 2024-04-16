db0b2853ae18fb64f644326d043213788d5d748e
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-04-17 01:39:05 +0530

    handel url

--- 06_node_js/03_server/index.js
+++ 06_node_js/03_server/index.js
@@ -1,13 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
 
 const myServer = http.createServer((req, res) => {
     // console.log(req);
     // console.log("new request")
+
+    if(req.url === '/favicon.ico') return res.end()
     const msg = `${Date.now()}: New Request on : ${req.url}\n`
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
     fs.appendFile('./test.txt', msg, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/':
                 res.end("Welcome to Home Page")
                 break;
@@ -15,7 +20,8 @@ const myServer = http.createServer((req, res) => {
                 res.end("Welcome to Login Page")
                 break;
             case '/about':
-                res.end("Welcome to About Page")
+                const username = myUrl.query.user
+                res.end(`Welcome to About Page, User: ${username}`)
                 break;
         
             default:
